d693054c5c3f5e19861fe055d29514e4f0e6176c
zovgoyda <comradegreen@yandex.ru>
2026-07-07 17:07:30 +0400

    Delete profiles/noctalia directory

--- profiles/noctalia/installer/steps/step_finish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-render_header "✅ Installation complete!"
-
-if ask_yes_no "Would you like to test your current theme?"; then
-  run_cmd sudo -u $SUDO_USER sddm-greeter --test-mode --theme $DEST_DIR
-fi
-
-render_subheader "Goodbye cruel world!"
