6160215549260f39d0d97c1a0c22c7b22463aeda
zovgoyda <comradegreen@yandex.ru>
2026-07-07 16:49:21 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ fi
 
 # 3. Установка пакетов из AUR
 echo "Установка пакетов из AUR (noctalia-shell-git, happ-desktop-bin, vesktop)..."
-paru -S noctalia-shell-git happ-desktop-bin vesktop-bin
+paru -S --needed noctalia-shell-git happ-desktop-bin vesktop-bin
 
 # 4. Установка темы Noctalia для SDDM
 echo "Установка темы Noctalia для SDDM..."
