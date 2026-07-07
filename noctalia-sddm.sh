948f861d608b8d71d9ca68b2f0417496f7104c70
zovgoyda <comradegreen@yandex.ru>
2026-07-07 17:00:26 +0400

    Update noctalia-sddm.sh

--- noctalia-sddm.sh
+++ noctalia-sddm.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+# Установка темы Noctalia для SDDM
+echo "Установка темы Noctalia для SDDM..."
+cd /tmp
+git clone https://github.com/mda-dev/noctalia-sddm-theme.git noctalia
+cd noctalia
+# Запуск установщика
+sudo bash ./installer/install.sh
+cd ..
+rm -rf noctalia
